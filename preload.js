7736c5d87f47d1e21852bc18a2b90b1f9335b765
林泉 <591928179@qq.com>
2020-10-19 02:43:08 +0800

    修改一下排版

--- preload.js
+++ preload.js
@@ -1,6 +1,9 @@
 window.exports = {
-	"copy_text": { // 注意：键对应的是 plugin.json 中的 features.code
-		mode: "none", // 用于无需 UI 显示，执行一些简单的代码
+	// 注意：键对应的是 plugin.json 中的 features.code
+	"copy_text": {
+		// 用于无需 UI 显示，执行一些简单的代码
+		// 除此之外，utools还提供了列表、文档等模式
+		mode: "none",
 		args: {
 			enter: (action) => {
 				// action = { code, type, payload }
